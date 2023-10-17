07cf56a2725fd4904a29d0044c56c000915594d2
JefferDevop <somadesarrollo@gmail.com>
2023-10-17 09:15:20 -0500

    Commit version 6.0

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+
+// export const BASE_API = `https://lekar.catalogointeractivo.com.co`;
+export const BASE_API = `https://cristaleriala10.catalogointeractivo.com.co`;
+
+
+export const SEARCH_HISTORY = "search-history";
+export const CART = "cart";
+export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/";
