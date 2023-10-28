1a07f800bc07963a67f7e9ca838e5bcf3065fccc
JefferDevop <somadesarrollo@gmail.com>
2023-10-28 02:24:37 -0500

    Variables de entorno

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,5 @@
 
-// export const BASE_API = `https://lekar.catalogointeractivo.com.co`;
-export const BASE_API = `https://cristaleriala10.catalogointeractivo.com.co`;
-
-
+export const BASE_API = NEXT_PUBLIC_API
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
-export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/";
+export const BASE_NAME = NEXT_PUBLIC_STORAGE
