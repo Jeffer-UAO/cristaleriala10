db7de7437431642169ccc953f14dd592505a73b4
JefferDevop <somadesarrollo@gmail.com>
2023-12-23 00:21:37 -0500

    Ajuste de margenes y tamaño de letras en ListProducts

--- src/components/ListCart/ListCart.js
+++ src/components/ListCart/ListCart.js
@@ -45,7 +45,7 @@ export function ListCart(props) {
             <div className={styles.detalle}>
               <p className={styles.name}>{item.name_extend}</p>
 
-              <p className={styles.price}>$. {format(item.price1)} </p>
+              <p className={styles.price}>$ {format(item.price1)} </p>
               {/* <p className={styles.price}>Mayor: $ {item.price2}</p> */}
 
 
