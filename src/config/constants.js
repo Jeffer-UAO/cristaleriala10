fad0541d9d63327c2956732d1a1f49374e636eca
JefferDevop <somadesarrollo@gmail.com>
2024-02-06 07:06:10 -0500

    Manejo de usuarios para precio por mayor

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,5 +2,6 @@
 export const BASE_API = process.env.NEXT_PUBLIC_API
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
+export const TOKEN = "token";
 export const BASE_NAME = process.env.NEXT_PUBLIC_STORAGE
 //
